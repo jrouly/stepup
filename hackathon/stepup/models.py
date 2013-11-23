3b9e2ee0b52dab04255dba3aac769a628cfab4d4
Daniel W Bond <dbond2@gmu.edu>
2013-11-22 23:25:30 -0500

    moved ordering on models, fixed indentation

--- hackathon/stepup/models.py
+++ hackathon/stepup/models.py
@@ -34,18 +34,21 @@ class Opportunity(User):
         return ('name_of_the_view', None, {'slug':self.slug})
 
 class Person(User):
-
+    
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    bio = models.TextField(max_length = 2048)
+    city = models.CharField(max_length = 50)
+    state = models.CharField(max_length = 50)
+    country models.CharField(max_length = 50)
+    schedule = models.DateField()
+    tags = models.ForeignKey('Tag')
+    
     @permalink
     def get_absolute_url(self):
         return ('name_of_the_view', None, {'slug':self.slug})
-	first_name = models.CharField(max_length = 200)
-	last_name = models.CharField(max_length = 200)
-	bio = models.TextField(max_length = 2048)
-	city = models.CharField(max_length = 50)
-	state = models.CharField(max_length = 50)
-	country = models.CharField(max_length = 50)
-	schedule = models.DateField()
-	tags = models.ForeignKey('Tag')
+	
+    tags = models.ForeignKey('Tag')
 
     def __unicode__(self):
 	return '%s' % self.name
