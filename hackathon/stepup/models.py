dc263fbc9dc6a994de29fe69502f8fb78d6318c7
hairyjewbear <bsawyerwaters@gmail.com>
2013-11-22 22:45:17 -0500

    fixed models

--- hackathon/stepup/models.py
+++ hackathon/stepup/models.py
@@ -21,28 +21,23 @@ class Tag(models.Model):
     name = models.CharField(max_length = 200)
     description = models.TextField()
 
-<<<<<<< HEAD
     @permalink
     def get_absolute_url(self):
-        return ('name_of_the_view;, None, {'slug':self.slug})
+        return ('name_of_the_view', None, {'slug':self.slug})
      
-=======
     def __unicode__(self):
         return '%s' % self.name
->>>>>>> 8d2caacf150bd42121359d75f0df410a745c5b90
 class Opportunity(User):
 
     @permalink
     def get_absolute_url(self):
-        return ('name_of_the_view;, None, {'slug':self.slug})
+        return ('name_of_the_view', None, {'slug':self.slug})
 
 class Person(User):
 
-<<<<<<< .merge_file_ZutE4E
     @permalink
     def get_absolute_url(self):
-        return ('name_of_the_view;, None, {'slug':self.slug})
-=======
+        return ('name_of_the_view', None, {'slug':self.slug})
 	first_name = models.CharField(max_length = 200)
 	last_name = models.CharField(max_length = 200)
 	bio = models.TextField(max_length = 2048)
@@ -54,7 +49,6 @@ class Person(User):
 
 	def __unicode__(self):
 		return '%s' % self.name
->>>>>>> .merge_file_7TotPB
 
 class Organization(User):
 
