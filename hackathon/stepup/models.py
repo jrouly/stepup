5059256b8b9f31f8c362c122d85b517d8eeac4a4
Daniel W Bond <dbond2@gmu.edu>
2013-11-22 21:52:40 -0500

    added the first thing to the models-- everyone just came back, so just pushing for fun

--- hackathon/stepup/models.py
+++ hackathon/stepup/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """Default fields
+    """
+
+    name = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length = 50)
+    # location =    
+    description = models.TextField()
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+    class Meta:
+        abstract = True
+
+class Tag(models.Model):
+
+    
+class Opportunity(User):
+
+class Person(User):
+
+class Organization(User):
