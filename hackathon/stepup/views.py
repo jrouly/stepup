b846dc56a573e0fb786a17cbdf8bf2ce38136442
Daniel W Bond <dbond2@gmu.edu>
2013-11-23 12:18:11 -0500

    added requestcontext

--- hackathon/stepup/views.py
+++ hackathon/stepup/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
 # other view functions
 
 #login
@@ -54,25 +56,31 @@ def index(request):
     return render_to_response('index.html', {
 
     },
+    context_instance = RequestContext(request),
     )
+
 @login_required
 def opportunity(request, slug):
-
     return render_to_response('opportunity.html', {
         # 'name' : 
     },
+    context_instance = RequestContext(request),
     )
+
 @login_required
 def person(request, slug):
     return render_to_response('person.html', {
     
     },
+    context_instance = RequestContext(request),
     )
+
 @login_required
 def organization(request, slug):
     return render_to_response('organization.html', {
     # put the variables you need here
     },
+    context_instance = RequestContext(request),
     )
 
 def about(request):
