34dcd3801c989dd30cc28b0f5f5f0fdd5112d308
Daniel W Bond <dbond2@gmu.edu>
2013-11-22 23:08:47 -0500

    added pagination to the views

--- hackathon/stepup/views.py
+++ hackathon/stepup/views.py
@@ -1,11 +1,27 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
 
 # other view functions
 
 # webpage-generating view functions
 def index(request):
+
+    opportunities = Opportunity.objects.filter(posted__year=year, posted__month=month)
+
+    paginator = Paginator(opportunities, 12)
+
+    try:
+         page = int(request.GET.get("page", '1'))
+    except:
+         ValueError: page = 1
+
+    try: 
+        opportunities = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        opportunities = paginator.page(paginator.num_pages)
 
     return render_to_response('index.html', {
 
@@ -15,7 +31,7 @@ def index(request):
 def opportunity(request):
 
     return render_to_response('opportunity.html', {
-
+    
     },
     )
 
