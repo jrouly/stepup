328a7853fb1b08c8cfba3b73d5efd99b81af1310
Daniel W Bond <dbond2@gmu.edu>
2013-11-22 22:03:51 -0500

    added permalinks

--- hackathon/stepup/models.py
+++ hackathon/stepup/models.py
@@ -19,9 +19,24 @@ class User(models.Model):
 
 class Tag(models.Model):
 
-    
+    @permalink
+    def get_absolute_url(self):
+        return ('name_of_the_view;, None, {'slug':self.slug})
+     
 class Opportunity(User):
+
+    @permalink
+    def get_absolute_url(self):
+        return ('name_of_the_view;, None, {'slug':self.slug})
 
 class Person(User):
 
+    @permalink
+    def get_absolute_url(self):
+        return ('name_of_the_view;, None, {'slug':self.slug})
+
 class Organization(User):
+
+    @permalink
+    def get_absolute_url(self):
+        return ('name_of_the_view;, None, {'slug':self.slug})
