9bd7649c8fcb8ca82083fa99c3807854e2347b43
Ben Waters <master@ubuntu.(none)>
2013-11-22 22:11:41 -0500

    edited person model

--- hackathon/stepup/models.py
+++ hackathon/stepup/models.py
@@ -24,4 +24,16 @@ class Opportunity(User):
 
 class Person(User):
 
+	first_name = models.CharField(max_length = 200)
+	last_name = models.CharField(max_length = 200)
+	bio = models.TextField(max_length = 2048)
+	city = models.CharField(max_length = 50)
+	state = models.CharField(max_length = 50)
+	country models.CharField(max_length = 50)
+	schedule = models.DateField()
+	tags = models.ForeignKey('Tag')
+
+	def __unicode__(self):
+		return '%s' % self.name
+
 class Organization(User):
